d6a1a9fc5acb0480775bff44d565b757883dbd06
ALAA DIB <dibalaa.05@gmail.com>
2023-11-07 19:58:03 +0100

    add drawDisc function

--- src/Turtle.py
+++ src/Turtle.py
@@ -1,4 +1,6 @@
 import turtle
+# from src.init import *
+# from src.config import N
 
 # Global constants
 TOWER_THICKNESS = 10
@@ -57,3 +59,49 @@ def drawBoard(n):
     return win
 
 drawBoard(3)
+
+# Function to draw a disc at a specific position on the board
+def drawDisc(nd, board, n):
+    # Configuration constants
+    TOWER_THICKNESS = 10
+    PLATE_THICKNESS = 20
+
+    # Initialize the turtle for drawing the disc
+    disc_drawer = turtle.Turtle()
+    disc_drawer.speed(0)
+    disc_drawer.penup()
+
+    # Find the tower and disk position on the board
+    tower_index = None
+    disk_index = None
+
+    for i in range(len(board)):
+        if nd in board[i]:
+            tower_index = i
+            disk_index = board[i].index(nd)
+            break
+
+    # Calculate the coordinates to draw the disk
+    x_position = tower_index * (TOWER_THICKNESS + PLATE_THICKNESS)
+    y_position = disk_index * PLATE_THICKNESS
+
+    # Move the turtle to the calculated position
+    disc_drawer.goto(x_position, y_position)
+
+    # Draw the disk
+    disc_drawer.pendown()
+    disc_drawer.pensize(2)
+
+    disc_drawer.forward(PLATE_THICKNESS)
+    disc_drawer.left(90)
+    disc_drawer.forward(2 * (n + 1) * PLATE_THICKNESS)
+    disc_drawer.left(90)
+    disc_drawer.forward(PLATE_THICKNESS)
+    disc_drawer.left(90)
+    disc_drawer.forward(2 * (n + 1) * PLATE_THICKNESS)
+    disc_drawer.left(90)
+
+    turtle.done()
+
+# Example usage
+drawDisc(2, [[3, 2], [1], []], 3)
