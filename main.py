8265c7e401e6746bb28d002e20fd9d01d8682beb
Alex Lovser <tabalex2005@gmail.com>
2023-11-12 21:48:06 +0100

    sounds main

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-
-from copy import deepcopy
-
 from src.init import *
 from src.config import N
-from src.operations import moveDisque
 
 from src.cheating.types.render_manager import RenderManager
 from src.cheating.types.event_manager import EventManager
@@ -12,39 +8,18 @@ from src.config import *
 from src.operations import *
 
 
-
-# moveDisque(plateau, 0, 1)
-# moveDisque(plateau, 0, 2)
-# moveDisque(plateau, 0, 3)
-# moveDisque(plateau, 0, 4)
-
-# moveDisque(plateau, 1, 2)
-# moveDisque(plateau, 4, 1)
-# moveDisque(plateau, 0, 4)
-# moveDisque(plateau, 1, 0)
-
-# moveDisque(plateau, 0, 4)
-# moveDisque(plateau, 3, 4)
-# moveDisque(plateau, 2, 0)
-# moveDisque(plateau, 2, 4)
-# moveDisque(plateau, 0, 4)
-
-
-# print(PLATEAU)
-# print(plateau)
-# print(verifVictoire(plateau, N))
-
-
 def run():
     context = {
+        'is_interaction': True,
         "mouse_x": 0,
         "mouse_y": 0,
         "dragging": None,
         "window_width": RenderManager.window.window_width(),
         "window_height": RenderManager.window.window_height(),
-        'disk_number': N + 5 , # or more
+        'disk_number': N + 2 , # or more
         'disk_colors': {},
         'disk_colors_adjusted': {},
+        'is_victory': False
         
     }
 
@@ -53,7 +28,6 @@ def run():
     for i in range(context['disk_number']):
         context['disk_colors'][i + 1] = generateDiskColor(context, i + 1)
         context['disk_colors_adjusted'][i + 1] = adjust_color_lighten(*hex_to_rgb(context['disk_colors'][i + 1]), 0.4)
-        # context['disk_colors_adjusted'][i + 1] = '#000'
 
 
     RenderManager.start_render(context)
