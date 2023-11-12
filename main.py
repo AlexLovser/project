c3dcb2b43176f5cebeb5d9c719dd45123ee46c58
Alex Lovser <tabalex2005@gmail.com>
2023-11-12 17:46:24 +0100

    .

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from src.cheating.types.render_manager import RenderManager
 from src.cheating.types.event_manager import EventManager
 
 from src.config import *
-from src.operations import generateDiskColor
+from src.operations import *
 
 
 
@@ -41,15 +41,17 @@ def run():
         "dragging": None,
         "window_width": RenderManager.window.window_width(),
         "window_height": RenderManager.window.window_height(),
-        'disk_number': N + 2, # or more
+        'disk_number': N , # or more
         'disk_colors': {},
-        
+        'disk_colors_adjusted': {},
     }
 
     context['board'] = init(context['disk_number'])
 
     for i in range(context['disk_number']):
         context['disk_colors'][i + 1] = generateDiskColor(context, i + 1)
+        context['disk_colors_adjusted'][i + 1] = adjust_color_lighten(*hex_to_rgb(context['disk_colors'][i + 1]), 0.4)
+        # context['disk_colors_adjusted'][i + 1] = '#000'
 
     RenderManager.is_interaction = True
     RenderManager.start_render(context)
