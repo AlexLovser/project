6d7dc72dc04be02c508d0138d1d1d508d240e0ee
Alex Lovser <tabalex2005@gmail.com>
2023-11-12 14:26:36 +0100

    turn turtlepy back

--- src/Turtle.py
+++ src/Turtle.py
@@ -4,18 +4,65 @@ from config import N
 from random import randint
 colors = ["#1ecbe1", "#2684d9", "#2960d6", "#243ddb", "#3023dc", "#4823dc", "#6220df", "#831ee1"]
 
+def drawBoard(n):
+    turtle.update()
+    turtle.speed(0)
+    turtle.penup()
+    turtle.goto(0,150)
+    turtle.pendown()
+    turtle.color("white")
+    turtle.write("Tour d'Hanoï",align="center",font=("ariel",48,"bold"))
+    turtle.color("white")
+    turtle.hideturtle()
 
-def drawDisc(nd, board, n, color):
+
+    dr = turtle.Turtle()
+    dr.speed(0)
+    dr.penup()
+    win = turtle.Screen()
+    x, y = 900, 600
+
+    win.setup(x,y)
+    win.bgcolor("#8da11d")
+    dr.color("white")
+    dr.pensize(4)
+    dr.goto(-x/2 + 50,-y/4)
+    dr.pendown()
+    turtle.update()
+
+
+    for i in range (3):
+        dr.forward(n * 25)
+        dr.forward(-n * 12.5)
+        dr.left(90)
+        dr.forward(200)
+        dr.forward(-200)
+        dr.left(-90)
+        dr.penup()
+        dr.left(-90)
+        dr.forward(25)
+        dr.write(f"Tower #{i+1}",align="center",font=("ariel",14,"bold"))
+        dr.forward(-25)
+        dr.left(90)
+        dr.forward(n * 25)
+        dr.pendown()
+    
+
+    turtle.hideturtle()
+    turtle.done()
+
+def drawDisc(nd, board, n):
     turtle.update()
     disc_turtle = turtle.Turtle()
     disc_turtle.speed(0)
     disc_turtle.penup()
     disc_turtle.pensize(20)
+    i in colors
     disc_turtle.pencolor(colors[i-1])
     tower_number = posDisque(board, nd)
     tower_height = nbDisques(board, tower_number)
     disc_size = n  + 1 
-    x = -400 + nd * 12 # + ((nd - 1) * 43) * 0 # the final number is the tower number - 1
+    x = (-400) + (nd*12 ) + ((nd-1) * 43) * 0# the final number is the tower number - 1
     y = -133 - n * 21 + 20 * tower_height
     
     disc_turtle.goto(x , y)
@@ -59,18 +106,16 @@ def drawConfig(board, n):
 
 def resetConfig(board, n):
     tower_number = posDisque(board, n)
-    # tower_height = nbDisques(board, tower_number)
-    
-    for i in range(n):
-        eraseDisc(i, board, n + 1)
+    tower_height = nbDisques(board, tower_number)
+    for i in range(1,n+1):
+        eraseDisc(i,board,n)
     drawBoard(n)
 
-n = 8
-for i in range(n):
-    drawDisc(n + 1, init(n + 1), i)
-
+n=8
+for i in range(1,n+1):
+    drawDisc(n, init(n), i)
 # drawBoard(n)
-eraseDisc(1, init(8), 8)
+eraseDisc(1,init(8),8)
 
 drawConfig(init(n),n)
 # resetConfig(init(n),n)
