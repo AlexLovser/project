1d441d6e3392da8bec3d74dabc27d4c619349244
ALAA DIB <dibalaa.05@gmail.com>
2023-11-14 10:17:15 +0100

    add history functions

--- src/history.py
+++ src/history.py
@@ -1,15 +1,19 @@
+from src.operations import moveDisque
 
 
 
-def add_movement_to_history(context):
-    ...
+def add_movement_to_history(context, de , a):
+    moveDisque(context['board'],de, a)
+    context['history'].append([de, a])
 
 
 def undo_last_movement(context):
-    ...
+    if context['history'] != None:
+        de, a= context['history'].pop()
+        moveDisque(context['board', a, de])
+
 
 
 def view_history(context):
-    ...
-
-
+    for i in range(len(context['history'])):
+        print("De:",context['history'][0]," à:",context['history'][1])
